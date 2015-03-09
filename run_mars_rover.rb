c53f873a9ced836f92d69f848e0dafe6fecd99f0
Divyum <divyum@Divyums-MacBook-Air.local>
2015-03-09 15:47:16 +0530

    updated

--- run_mars_rover.rb
+++ run_mars_rover.rb
@@ -1,13 +1,14 @@
 require_relative 'mars_rover'
-mat_x, mat_y = gets.chomp.split()
+mat_x, mat_y = gets.chomp.split
 Rover.mat_size(Integer(mat_x), Integer(mat_y))
 while true
-	x ,y, pos = gets.chomp.split()
+	x ,y, pos = gets.chomp.split
 	break if x==nil
-	x, y = Integer(x), Integer(y)
-	seq = gets.chomp.split()[0]
+	#raise "Invalid Input" if x!=nil and y==nil
 	rover = Rover.new(x, y, pos)
-	rover.sequence(seq)
-	rover.start
+	seq = gets.chomp.split[0]
+	if rover.sequence(seq)
+		rover.start
+	end
 	print rover.position[0], ' ', rover.position[1], ' ', rover.position[2], "\n" 
 end
