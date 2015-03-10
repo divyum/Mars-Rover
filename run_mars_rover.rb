a87c28edeab7f850ae6747e3319d67898a8e1530
Divyum <divyum@Divyums-MacBook-Air.local>
2015-03-10 16:18:53 +0530

    exceptions don't halt the rover

--- run_mars_rover.rb
+++ run_mars_rover.rb
@@ -1,14 +1,27 @@
 require_relative 'mars_rover'
+begin
 mat_x, mat_y = gets.chomp.split
-Rover.mat_size(Integer(mat_x), Integer(mat_y))
+Rover.mat_size(mat_x, mat_y)
+rescue InputError => e
+	puts e.message
+	retry
+end
+
 while true
-	x ,y, pos = gets.chomp.split
-	break if x==nil
-	#raise "Invalid Input" if x!=nil and y==nil
-	rover = Rover.new(x, y, pos)
-	seq = gets.chomp.split[0]
-	if rover.sequence(seq)
-		rover.start
+	begin
+		x ,y, pos = gets.chomp.split
+		rover = Rover.new(x, y, pos)
+		#break if x==nil
+	rescue InputError => e
+		puts e.message
+		retry
 	end
-	print rover.position[0], ' ', rover.position[1], ' ', rover.position[2], "\n" 
+	begin
+		seq = gets.chomp.split[0]
+		rover.start if rover.sequence(seq)
+		print rover.position[0], ' ', rover.position[1], ' ', rover.position[2], "\n" 
+	rescue SequenceError => e
+		puts e.message
+		retry
+	end
 end
